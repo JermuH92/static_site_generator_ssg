fab2f8333d1e08ba8b471b5d1e94d12a1a20c499
JermuH92 <jtkukkohovi@hotmail.com>
2026-05-15 18:05:05 +0300

    Initial commit: Setup project folders (public, src) and files. Create enums for text types and start building the TextNode, Create: main.py to test and script to run the file.

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from textnode import TextNode
+
+def main():
+
+    new_node = TextNode("This is some text", "link", "https://www.boot.dev")
+    print(new_node)
+
+if __name__ == "__main__":
+    main()
