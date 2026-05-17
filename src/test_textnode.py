53e91b6da93f4d99a5030d87c2be1324a1312bb8
JermuH92 <jtkukkohovi@hotmail.com>
2026-05-17 20:04:04 +0300

    Feat: Implement a text node to html function, which checks for a match of enums to text node and returns a LeafNode with their correct tags, texts and properties. Build additional test cases in test_textnode.py to check that the new function works properly.

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -22,7 +22,51 @@ class TestTextNode(unittest.TestCase):
     def test_repr(self):
         node = TextNode("This is even more text", TextType.CODE_TEXT, "https://example.com")
         self.assertEqual(repr(node), "TextNode(This is even more text, code, https://example.com)")
+    
+    def test_plain_text(self):
+        node = TextNode("This is a text node", TextType.TEXT_PLAIN)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+    
+    def test_tags(self):
+        node = TextNode("This is bold text", TextType.BOLD_TEXT)
+        node2 = TextNode("This is italic text", TextType.ITALIC_TEXT)
+        node3 = TextNode("This is code text", TextType.CODE_TEXT)
 
+        html_node = text_node_to_html_node(node)
+        html_node2 = text_node_to_html_node(node2)
+        html_node3 = text_node_to_html_node(node3)
+
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is bold text")
+
+        self.assertEqual(html_node2.tag, "i")
+        self.assertEqual(html_node2.value, "This is italic text")
+
+        self.assertEqual(html_node3.tag, "code")
+        self.assertEqual(html_node3.value, "This is code text")
+    
+    def test_link_img_tags(self):
+        node_link = TextNode("This is a link text", TextType.LINK_TEXT, "https://boot.dev")
+        node_image = TextNode("This is an image", TextType.IMAGE_TEXT, "https://notareal.pic.pic.com/picture.png")
+
+        html_node = text_node_to_html_node(node_link)
+        html_node2 = text_node_to_html_node(node_image)
+
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "This is a link text")
+
+        self.assertEqual(html_node2.tag, "img")
+        self.assertEqual(html_node2.value, "")
+
+        self.assertEqual(html_node.props, {"href": "https://boot.dev"})
+        self.assertEqual(html_node2.props, {"src": "https://notareal.pic.pic.com/picture.png", "alt": "This is an image"})
+    
+    def test_no_enum_match(self):
+        node = TextNode("Text", "TextType.NOT_REAL_ENUM")
+        with self.assertRaises(ValueError):
+            text_node_to_html_node(node)
     
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 class TextType(Enum):
     TEXT_PLAIN = "text"
@@ -20,4 +21,29 @@ class TextNode:
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
 
+
+def text_node_to_html_node(text_node):
+    
+    match text_node.text_type:
+
+        case TextType.TEXT_PLAIN:
+            return LeafNode(None, text_node.text)
+    
+        case TextType.BOLD_TEXT:
+            return LeafNode("b", text_node.text)
+
+        case TextType.ITALIC_TEXT:
+            return LeafNode("i", text_node.text)
+    
+        case TextType.CODE_TEXT:
+            return LeafNode("code", text_node.text)
+    
+        case TextType.LINK_TEXT:
+            return LeafNode("a", text_node.text, {"href": text_node.url})
+
+        case TextType.IMAGE_TEXT:
+            return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+    
+        case _:
+            raise ValueError("Error: TextNode text type doesn't match any defined Enum-type.")
 
