2bce3e498a2ca352d6728a84731bb0f5a7603b6c
JermuH92 <jtkukkohovi@hotmail.com>
2026-05-21 00:12:33 +0300

    Feat: Implement comprehensive unit tests to cover both new split_image and split_link functions.

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -81,21 +81,168 @@ class TestSplitNodesDelimiter(unittest.TestCase):
 ########### SPLIT_NODES_IMAGE CLASS STARTS HERE ###########
 
 class TestSplitNodesImage(unittest.TestCase):
-
-    def test_split_images(self):
+    
+    def test_split_images_single(self):
+        node = TextNode("![image one](https://localhost.first.png)", TextType.PLAIN_TEXT,)
+        new_node = split_nodes_image([node])
+        self.assertListEqual([TextNode("image one", TextType.IMAGE_TEXT, "https://localhost.first.png")], new_node)
+        
+    def test_split_images_multiple(self):
         node = TextNode(
-            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            "First text ![image one](https://localhost.first.png) second text ![image two](https://localhost.second.png) third text",
             TextType.PLAIN_TEXT,
         )
         new_nodes = split_nodes_image([node])
         self.assertListEqual(
         [
-            TextNode("This is text with an ", TextType.PLAIN_TEXT),
-            TextNode("image", TextType.IMAGE_TEXT, "https://i.imgur.com/zjjcJKZ.png"),
-            TextNode(" and another ", TextType.PLAIN_TEXT),
-            TextNode(
-                "second image", TextType.IMAGE_TEXT, "https://i.imgur.com/3elNhQu.png"
-            ),
+            TextNode("First text ", TextType.PLAIN_TEXT),
+            TextNode("image one", TextType.IMAGE_TEXT, "https://localhost.first.png"),
+            TextNode(" second text ", TextType.PLAIN_TEXT),
+            TextNode("image two", TextType.IMAGE_TEXT, "https://localhost.second.png"),
+            TextNode(" third text", TextType.PLAIN_TEXT),
+        ],
+        new_nodes,
+    )
+    
+    def test_split_images_mid_string(self):
+        node = TextNode("Test picture ![cat](https://test.cat.pictures.cat/cat.png) in the middle", TextType.PLAIN_TEXT, )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+        [
+            TextNode("Test picture ", TextType.PLAIN_TEXT),
+            TextNode("cat", TextType.IMAGE_TEXT, "https://test.cat.pictures.cat/cat.png"),
+            TextNode(" in the middle", TextType.PLAIN_TEXT)
+        ],
+        new_nodes
+    )
+    
+    def test_split_images_no_empty_nodes(self):
+        node = TextNode(
+            "![cat](https://test.cat.pictures.cat/cat.png) text only in middle ![dog](https://test.dog.pictures.dog/dog.png)",
+            TextType.PLAIN_TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+        [
+            TextNode("cat", TextType.IMAGE_TEXT, "https://test.cat.pictures.cat/cat.png"),
+            TextNode(" text only in middle ", TextType.PLAIN_TEXT),
+            TextNode("dog", TextType.IMAGE_TEXT, "https://test.dog.pictures.dog/dog.png"),
+        ],
+        new_nodes
+    )
+    
+    def test_split_images_consecutive(self):
+        node = TextNode("![cat](https://test.cat.pictures.cat/cat.png)![dog](https://test.dog.pictures.dog/dog.png)", TextType.PLAIN_TEXT, )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+        [
+            TextNode("cat", TextType.IMAGE_TEXT, "https://test.cat.pictures.cat/cat.png"),
+            TextNode("dog", TextType.IMAGE_TEXT, "https://test.dog.pictures.dog/dog.png"),
+        ],
+        new_nodes
+    )
+    
+    def test_split_images_non_plain_passthrough(self):
+        node = TextNode("This nodes text is **bold**", TextType.BOLD_TEXT)
+        new_node = split_nodes_image([node])
+        self.assertListEqual([TextNode("This nodes text is **bold**", TextType.BOLD_TEXT)], new_node)
+        
+    def test_split_images_no_images_return_original(self):
+        node = TextNode("This node is just plain text", TextType.PLAIN_TEXT)
+        new_node = split_nodes_image([node])
+        self.assertListEqual([TextNode("This node is just plain text", TextType.PLAIN_TEXT)], new_node)
+
+########### SPLIT_NODES_IMAGE CLASS ENDS HERE ###########
+
+########### SPLIT_NODES_LINK CLASS STARTS HERE ###########
+
+class TestSplitNodesLink(unittest.TestCase):
+
+    def test_split_links_no_alt_text(self):
+        node = TextNode("[](https://www.google.com)", TextType.PLAIN_TEXT,)
+        new_node = split_nodes_link([node])
+        self.assertListEqual([TextNode("", TextType.LINK_TEXT, "https://www.google.com")], new_node)
+
+    def test_split_links_single(self):
+        node = TextNode("[link to google](https://www.google.com)", TextType.PLAIN_TEXT,)
+        new_node = split_nodes_link([node])
+        self.assertListEqual([TextNode("link to google", TextType.LINK_TEXT, "https://www.google.com")], new_node)
+    
+    def test_split_links_multiple(self):
+        node = TextNode(
+            "First link text [enter site](https://www.youtube.com) second link text [enter site two](https://www.google.com) third link text",
+            TextType.PLAIN_TEXT,
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+        [
+            TextNode("First link text ", TextType.PLAIN_TEXT),
+            TextNode("enter site", TextType.LINK_TEXT, "https://www.youtube.com"),
+            TextNode(" second link text ", TextType.PLAIN_TEXT),
+            TextNode("enter site two", TextType.LINK_TEXT, "https://www.google.com"),
+            TextNode(" third link text", TextType.PLAIN_TEXT),
+        ],
+        new_nodes,
+    )
+    
+    def test_split_links_mid_string(self):
+        node = TextNode("Test link [to dashboard](https://boot.dev/dashboard) in the middle", TextType.PLAIN_TEXT, )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+        [
+            TextNode("Test link ", TextType.PLAIN_TEXT),
+            TextNode("to dashboard", TextType.LINK_TEXT, "https://boot.dev/dashboard"),
+            TextNode(" in the middle", TextType.PLAIN_TEXT)
+        ],
+        new_nodes
+    )
+    
+    def test_split_links_no_empty_nodes(self):
+        node = TextNode(
+            "[link one](https://notanactualwebsite.web) text only in middle [link two](https://notarealsite.forreal.net)",
+            TextType.PLAIN_TEXT,
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+        [
+            TextNode("link one", TextType.LINK_TEXT, "https://notanactualwebsite.web"),
+            TextNode(" text only in middle ", TextType.PLAIN_TEXT),
+            TextNode("link two", TextType.LINK_TEXT, "https://notarealsite.forreal.net"),
+        ],
+        new_nodes
+    )
+    
+    def test_split_links_consecutive(self):
+        node = TextNode("[link](https://google.com)[link2](https://boot.dev)", TextType.PLAIN_TEXT, )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+        [
+            TextNode("link", TextType.LINK_TEXT, "https://google.com"),
+            TextNode("link2", TextType.LINK_TEXT, "https://boot.dev"),
+        ],
+        new_nodes
+    )
+    
+    def test_split_links_non_plain_passthrough(self):
+        node = TextNode("This nodes text is **bold**", TextType.BOLD_TEXT)
+        new_node = split_nodes_link([node])
+        self.assertListEqual([TextNode("This nodes text is **bold**", TextType.BOLD_TEXT)], new_node)
+        
+    def test_split_links_no_links_return_original(self):
+        node = TextNode("This node is just plain text", TextType.PLAIN_TEXT)
+        new_node = split_nodes_link([node])
+        self.assertListEqual([TextNode("This node is just plain text", TextType.PLAIN_TEXT)], new_node)
+    
+    def test_split_links_ignores_images(self):
+        node = TextNode(
+            "This node contains both link [link](https://google.com) and image ![cat](https://test.cat.pictures.cat/cat.png)", TextType.PLAIN_TEXT,
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+        [
+            TextNode("This node contains both link ", TextType.PLAIN_TEXT),
+            TextNode("link", TextType.LINK_TEXT, "https://google.com"),
+            TextNode(" and image ![cat](https://test.cat.pictures.cat/cat.png)", TextType.PLAIN_TEXT),
         ],
         new_nodes,
     )
