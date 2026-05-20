36eab17a64e7d0d3e5ee1d74235d41f4771f30f1
JermuH92 <jtkukkohovi@hotmail.com>
2026-05-20 21:09:58 +0300

    Feat: Build and implement split_node functions for image and link processing,Refactor: Change file name to inline_markdown to match with its test file and include all inline functions in same file. Re-organize the markdown test file in function-based classes and comments for start and end to test case sections.

---
+++ src/inline_markdown.py
@@ -0,0 +1,121 @@
+import re
+from textnode import TextType, TextNode
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+
+    for old_node in old_nodes:
+
+        if old_node.text_type is not TextType.PLAIN_TEXT:
+            new_nodes.append(old_node)
+            continue
+            
+        current_text = old_node.text
+        split_text = current_text.split(delimiter)
+
+        if len(split_text) % 2 == 0:
+            raise Exception("Error: Delimiter is not matched or is not valid Markdown syntax.")
+
+        for index, value in enumerate(split_text):
+            if value == "":
+                continue
+
+            if index % 2 == 0:
+                new_nodes.append(TextNode(value, TextType.PLAIN_TEXT))
+
+            else:
+                new_nodes.append(TextNode(value, text_type))
+            
+    return new_nodes
+
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+
+    for old_node in old_nodes:
+
+        if old_node.text_type is not TextType.PLAIN_TEXT:
+            new_nodes.append(old_node)
+            continue
+
+        if old_node.text == "":
+            continue
+
+        original_text = old_node.text
+        images = extract_markdown_images(original_text)
+
+        if len(images) == 0:
+            new_nodes.append(old_node)
+            continue
+
+        for image_alt, image_link in images:
+
+            image_link_markdown = f"![{image_alt}]({image_link})"
+            sections = original_text.split(image_link_markdown, 1)
+
+            if len(sections) != 2:
+                raise ValueError("Invalid markdown, image section not closed.")
+
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], TextType.PLAIN_TEXT))
+            
+            new_nodes.append(TextNode(image_alt, TextType.IMAGE_TEXT, image_link))
+            original_text = sections[1]
+
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, TextType.PLAIN_TEXT))
+    
+    return new_nodes
+
+
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+
+    for old_node in old_nodes:
+
+        if old_node.text_type is not TextType.PLAIN_TEXT:
+            new_nodes.append(old_node)
+            continue
+
+        if old_node.text == "":
+            continue
+
+        original_text = old_node.text
+        links = extract_markdown_links(original_text)
+
+        if len(links) == 0:
+            new_nodes.append(old_node)
+            continue
+
+        for link_text, link in links:
+
+            link_markdown = f"[{link_text}]({link})"
+            sections = original_text.split(link_markdown, 1)
+
+            if len(sections) != 2:
+                raise ValueError("Invalid markdown, link section not closed.")
+
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], TextType.PLAIN_TEXT))
+            
+            new_nodes.append(TextNode(link_text, TextType.LINK_TEXT, link))
+            original_text = sections[1]
+
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, TextType.PLAIN_TEXT))
+    
+    return new_nodes
+
+
+
+def extract_markdown_images(text):
+    regex_matches_image = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+    return regex_matches_image
+
+
+def extract_markdown_links(text):
+    regex_matches_link = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+    return regex_matches_link
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,8 +1,17 @@
 import unittest
-from split_delimiter import split_nodes_delimiter
+from inline_markdown import (
+    split_nodes_delimiter,
+    split_nodes_image,
+    split_nodes_link,
+    extract_markdown_links,
+    extract_markdown_images,
+)
 from textnode import TextNode, TextType
 
-class TestInlineMarkdown(unittest.TestCase):
+########### SPLIT_NODES_DELIMITER CLASS STARTS HERE ###########
+
+class TestSplitNodesDelimiter(unittest.TestCase):
+
     def test_inline_uneven_delimiter(self):
         node = TextNode("This is a test block for missing **bold delimiter.", TextType.PLAIN_TEXT)
         with self.assertRaises(Exception) as context:
@@ -66,6 +75,77 @@ class TestInlineMarkdown(unittest.TestCase):
         result = split_nodes_delimiter([node], "**", TextType.BOLD_TEXT)
         expected = [TextNode("already bold", TextType.BOLD_TEXT)]
         self.assertListEqual(result, expected)
+
+########### SPLIT_NODES_DELIMITER CLASS ENDS HERE ###########
+
+########### SPLIT_NODES_IMAGE CLASS STARTS HERE ###########
+
+class TestSplitNodesImage(unittest.TestCase):
+
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.PLAIN_TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+        [
+            TextNode("This is text with an ", TextType.PLAIN_TEXT),
+            TextNode("image", TextType.IMAGE_TEXT, "https://i.imgur.com/zjjcJKZ.png"),
+            TextNode(" and another ", TextType.PLAIN_TEXT),
+            TextNode(
+                "second image", TextType.IMAGE_TEXT, "https://i.imgur.com/3elNhQu.png"
+            ),
+        ],
+        new_nodes,
+    )
+
+########### REGEX EXTRACTION CLASS STARTS HERE ###########
+
+class TestRegexExtraction(unittest.TestCase):
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
     
+    def test_extract_markdown_images_multiple(self):
+        matches = extract_markdown_images(
+            "![cat](https://cats.com/cat.png) and ![dog](https://dogs.com/dog.png)"
+        )
+        self.assertListEqual(
+            [("cat", "https://cats.com/cat.png"), ("dog", "https://dogs.com/dog.png")],
+            matches,
+        )
     
+    def test_extract_markdown_images_none(self):
+        matches = extract_markdown_images("This has no images at all")
+        self.assertListEqual([], matches)
     
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a link [to boot dev](https://www.boot.dev)"
+        )
+        self.assertListEqual([("to boot dev", "https://www.boot.dev")], matches)
+    
+    def test_extract_markdown_links_multiple(self):
+        matches = extract_markdown_links(
+            "[cat](https://cats.com) and [dog](https://dogs.com)"
+        )
+        self.assertListEqual(
+            [("cat", "https://cats.com"), ("dog", "https://dogs.com")],
+            matches,
+        )
+    
+    def test_extract_markdown_links_none(self):
+        matches = extract_markdown_links("This has no links at all")
+        self.assertListEqual([], matches)
+    
+    def test_extract_markdown_links_not_images(self):
+        matches = extract_markdown_links(
+            "![image](https://img.com/a.png) and [link](https://boot.dev)"
+        )
+        self.assertListEqual([("link", "https://boot.dev")], matches)
+    
+########### REGEX EXTRACTION CLASS ENDS HERE ###########    
