796fb49b93e6abc05dd6b87868e11d721894c8c7
JermuH92 <jtkukkohovi@hotmail.com>
2026-05-23 22:49:15 +0300

    Feat: Implement recursive static file copying
    Build a pipeline in main.py to handle static assets for the site generator.
    The process ensures a clean state by first deleting and recreating the
    destination directory ('public') before every build.
    
    Implement a copy_static_recursive function that utilizes a depth-first
    search (DFS) approach to traverse the source directory ('static').
    It successfully replicates the entire directory structure and copies
    all nested files to the destination using the 'os' and 'shutil' libraries.

--- src/main.py
+++ src/main.py
@@ -1,10 +1,41 @@
-from textnode import TextType, TextNode
-from htmlnode import HTMLNode
+import os
+import shutil
+
+
+def copy_static_recursive(source_path, destination_path):
+
+    items = os.listdir(source_path)
+
+    for item in items:
+
+        full_source_path = os.path.join(source_path, item)
+        full_destination_path = os.path.join(destination_path, item)
+
+        if os.path.isfile(full_source_path):
+            shutil.copy(full_source_path, full_destination_path)
+            print(f"Copied file: {full_source_path} -> {full_destination_path}")
+        
+        elif os.path.isdir(full_source_path):
+            os.mkdir(full_destination_path)
+            print(f"Created a directory: {full_destination_path}")
+
+            copy_static_recursive(full_source_path, full_destination_path)
 
 def main():
 
-    new_node = TextNode("This is some text", TextType.LINK_TEXT, "https://www.boot.dev")
-    print(new_node)
+    source_dir = "static"
+    destination_dir = "public"
+
+    print("Cleaning public folder...")
+
+    if os.path.exists(destination_dir):
+        shutil.rmtree(destination_dir)
+    os.mkdir(destination_dir)
+
+    print("Beginning recursive copying of static files...")
+    copy_static_recursive(source_dir, destination_dir)
+    print("Copying complete.")
+    
 
 
 if __name__ == "__main__":
